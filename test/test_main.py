1f891fcdd77c9a5583d5b256169739bab0be0c25
JHParker7 <111579699+JHParker7@users.noreply.github.com>
2024-02-16 09:46:56 +0000

    improved test

--- test/test_main.py
+++ test/test_main.py
@@ -4,10 +4,11 @@ import multiprocessing as par
 def get(x):
     return req.get(url="http://127.0.0.1:5000/get").content.decode()
 
-def load_balancer_stress(stress):
-    Pool=par.Pool(4)
+def load_balancer_stress(stress,threads):
+    Pool=par.Pool(threads)
     stress=[None for x in range(stress)]
     res=Pool.map(get,stress)
+    del Pool
     return res
 
 def test_load_balancer_work():
@@ -16,12 +17,18 @@ def test_load_balancer_work():
     
 def test_load_balancer_stress_2():
     stress=2
-    assert load_balancer_stress(stress) == ["hello world" for x in range(stress)]
+    assert load_balancer_stress(stress,2) == ["hello world" for x in range(stress)]
     
 def test_load_balancer_stress_4():
     stress=4
-    assert load_balancer_stress(stress) == ["hello world" for x in range(stress)]
+    assert load_balancer_stress(stress,4) == ["hello world" for x in range(stress)]
 
 def test_load_balancer_stress_10():
     stress=10
-    assert load_balancer_stress(stress) == ["hello world" for x in range(stress)]
+    assert load_balancer_stress(stress,4) == ["hello world" for x in range(stress)]
+    
+def test_load_balancer_stress_100():
+    stress=100
+    assert load_balancer_stress(stress,6) == ["hello world" for x in range(stress)]
+    
+
